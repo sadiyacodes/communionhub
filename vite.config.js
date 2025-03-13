daa050c4806d5670d849a0e8fd16cc0023f6739a
sadiyacodes <99393549+sadiyacodes@users.noreply.github.com>
2025-03-13 10:34:47 +0530

    routing fix for vercel

--- vite.config.js
+++ vite.config.js
@@ -9,4 +9,11 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  base: "/",
+  build: {
+    outDir: "dist",
+  },
+  server: {
+    historyApiFallback: true,
+  },
 });
